725eca852cc39ee1a20715ecf7dfd31a7c3d7369
nikitau <neavinstasi@icloud.com>
2023-07-17 14:40:50 +0300

    added ui-link (bug with refs)

---
+++ src/ui/MoreInfoLink/MoreInfoLink.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+import styles from './style/MoreInfoLink.module.css';
+
+
+interface Props{
+    src: string,
+    text: string
+}
+
+export const MoreInfoLink: FunctionComponent<Props> = (src, text) => {
+    return (
+        <a href={src.toString()} className={styles.link}>{text}</a>
+    );
+};
